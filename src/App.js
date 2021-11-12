a2546249a689828d40c072d938ccb561b0295037
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-11-12 22:41:51 +0300

    react-dom-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Movie from "./components/Movie";
 import Movie from "./components/Music";
+import Nav from "./components/Nav";
 const BASE_URL = "http://localhost:4000";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
      <Movie/>
      <Music/>
+     <Nav/>
     </div>
   );
 }
