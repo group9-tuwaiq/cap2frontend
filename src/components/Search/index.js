5797e0e11df83e35e6c33ef632ac2f13937f5d4e
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-11-14 10:06:34 +0300

    fix warnings

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,43 +1,19 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "http://";
 const Search = () => {
-
-        // eslint-disable-next-line
+  // eslint-disable-next-line
   const [result, setResult] = useState([]);
-          // eslint-disable-next-line
-        // eslint-disable-next-line
+  // eslint-disable-next-line
   const [media, setMedia] = useState([]);
-          // eslint-disable-next-line
+  // eslint-disable-next-line
   const search = async () => {
     const result = await axios.get(`${BASE_URL}/search`);
     setResult(result.data);
   };
   return <div></div>;
-
-  //console.log(result);
-  return (
-    <div className="Movie">
-      {/* <ul>
-        {result.map((item) => (
-          <>
-            <div>
-              <li key={item.trackId}>
-                {item.trackViewUrl}
-                {item.trackName}
-                <p>Created by :</p>
-                {item.artistName}
-              </li>
-            </div>
-            <button onClick={`()=>{addToFav(${item.trackId})}`}>
-              <i class="far fa-heart"></i>
-            </button>
-          </>
-        ))}
-      </ul> */}
-    </div>
-  );
-
 };
 
 export default Search;
