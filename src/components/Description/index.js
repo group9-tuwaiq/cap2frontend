fc581a2635689afb187d2f70688e04d92e9374ba
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-11-15 03:19:15 +0300

    fixes

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -1,22 +1,43 @@
 import React from "react";
-//import { useEffect, useState } from "react";
+import {  useState,useEffect } from "react";
+import axios from "axios";
+import {useParams} from "react-dom";
 
-const Description = ({ media }) => {
-  // const [item,setItem]=useState(media);
-  console.log(media);
+const BASE_URL = "http://localhost:4000";
+
+const Description = ({media}) => {
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    getCurrentItem();
+  }, []);
+
+  const getCurrentItem = async () => {
+    const info = await axios.get(`${BASE_URL}/getCurrentItem`);
+    console.log(info.data);
+    setItem(info.data);
+  };
   return (
     <div>
       {<>
-            <div>
-              <li key={media.trackId}>
-                {media.trackViewUrl}
-                {media.trackName}
+         <div>
+              <li key={item.trackId}>
+                {item.artworkUrl100}
+                {item.trackName}
                 <p>Created by :</p>
-                {media.artistName}
+                {item.artistName}   
+                <video
+                  // className={
+                  //   filteredPersons.kind == "feature-movie" ? "moviePlayer" : "player"
+                  // }
+                  controls
+                >
+                  <source src={item.previewUrl} type="video/mp4" />
+                </video>
               </li>
-            </div>
-            <button onClick={`()=>{addToFav(${media.trackId})}`}>
-              <i class="far fa-heart"></i>
+            </div> 
+             <button onClick={`()=>{addToFav(${item.trackId})}`}>
+              add to favorite
             </button>
           </>
         }
